80b475fecd7639b643637352d62eb7a25833c175
lcottret <ludovic.cottret@inrae.fr>
2020-09-07 18:05:52 +0200

    Change the location of the files for coverage

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/components/*.{js,vue}',
-    '!src/components/index.js'
+    'src/components/*/*.{js,vue}',
+    '!src/components/*/index.js'
   ],
   coverageReporters: ['html', 'text-summary']
 }
