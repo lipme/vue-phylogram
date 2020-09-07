0ddb012e38d378b88df1b87b53df81e32c1186b5
lcottret <ludovic.cottret@inrae.fr>
2020-09-07 10:27:49 +0200

    Simple snapshot tests for Phylogram

---
+++ tests/unit/phylogram.spec.js
@@ -0,0 +1,82 @@
+import { shallowMount } from '@vue/test-utils'
+import Phylogram from '@/components/Phylogram.vue'
+const jsonTree = {
+  id: 'ABCDE',
+  branchset: [
+    {
+      id: 'ABCD',
+      length: 0.2,
+      branchset: [
+        {
+          length: 0.3,
+          id: 'AB',
+          branchset: [
+            {
+              name: 'A',
+              id: 'A',
+              length: 0.2
+            },
+            {
+              id: 'B',
+              name: 'B',
+              length: 0.3
+            }
+          ]
+        },
+        {
+          length: 0.2,
+          id: 'CD',
+          name: 'CD',
+          branchset: [
+            {
+              id: 'C',
+              name: 'C',
+              length: 0.5
+            },
+            {
+              id: 'D',
+              name: 'D',
+              length: 0.3
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'E',
+      name: 'E',
+      length: 0.7
+    }
+  ]
+}
+
+const newick = '(((A:0.2, B:0.3):0.3,(C:0.5, D:0.3):0.2):0.3, E:0.7):1.0'
+
+describe('Phylogram component', () => {
+  it('does not render if no props inputTree or newick', () => {
+    const comp = shallowMount(Phylogram, {
+      propsData: {
+      }
+    })
+
+    expect(comp.element).toMatchSnapshot()
+  })
+  it('Tree from inputTree works', () => {
+    const comp = shallowMount(Phylogram, {
+      propsData: {
+        inputTree: jsonTree
+      }
+    })
+
+    expect(comp.element).toMatchSnapshot()
+  })
+  it('Tree from newick works', () => {
+    const comp = shallowMount(Phylogram, {
+      propsData: {
+        newick: newick
+      }
+    })
+
+    expect(comp.element).toMatchSnapshot()
+  })
+})
