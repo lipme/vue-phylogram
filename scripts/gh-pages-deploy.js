8bfedb55512cee9e9d058b7e575ff5e96b56d0ae
lcottret <ludovic.cottret@inrae.fr>
2023-01-31 11:27:39 +0100

    chore: change require to import in github actions

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require('execa')
-const fs = require('fs');
+const execa = import('execa')
+const fs = import('fs');
 (async () => {
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
